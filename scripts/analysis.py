996196657bb3f7081a1491bcbce8843464e80753
Chris Choi <mychoi314@gmail.com>
2023-06-20 18:05:51 -0400

    Downloads from the website, but issue with ghost table from website

---
+++ scripts/analysis.py
@@ -0,0 +1,19 @@
+import json
+
+with open('data/database.json', 'r') as f:
+    data = json.load(f)
+
+print(f'Total: {len(data["surname"])}')
+
+by_country = {}
+for i in range(len(data['surname'])):
+    item = {k: data[k][i] for k in data}
+    if item['country'] not in by_country:
+        by_country[item['country']] = []
+    by_country[item['country']].append(item)
+
+print(f'Total per country:')
+for country, items in by_country.items():
+    if country == 'united-states':
+        print(items)
+    print(f'{country}: {len(items)}')
